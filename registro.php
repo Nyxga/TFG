36bc31024aa1897cf98f21fadc25f7a10677078b
Nyxga <adrianmonte2010.am@gmail.com>
2024-10-23 12:00:59 +0200

    Alertas añadidas en página de registro.

--- registro.php
+++ registro.php
@@ -8,10 +8,60 @@
     <link rel="stylesheet" href="./css/estilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <style>
+            .alerta-fija {
+            position: fixed;
+            top: 20px;
+            left: 50%;
+            transform: translateX(-50%);
+            z-index: 1050;
+            width: 50%;
+            }
+        </style>
 </head>
 
 <body>
     <main>
+        <?php
+        session_start();
+        include 'conexion.php';
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $email = $_POST['email'];
+            $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+            $foto_default = './img/Usuario.svg';
+
+            if (strpos($email, '@orion.net') === false) {
+                $_SESSION['errorMensaje'] = 'El correo electrónico debe tener el dominio <strong>@orion.net</strong>';
+            } else {
+                try {
+                    $sql = "INSERT INTO EMPLEADOS (NOMBRE, APELLIDO, EMAIL, PASSWORD, FOTO) VALUES (?, ?, ?, ?, ?)";
+                    $stmt = $conexion->prepare($sql);
+                    $stmt->execute([$nombre, $apellido, $email, $password, $foto_default]);
+                    $_SESSION['successMensaje'] = 'El usuario <strong>' . $nombre . '</strong> ha sido registrado con éxito';
+                } catch (PDOException $e) {
+                    echo 'Error: ' . $e->getMessage();
+                }
+            }
+        }
+
+        if (isset($_SESSION['errorMensaje'])) {
+            echo '<div class="alert alert-danger text-center alerta-fija" role="alert">';
+            echo $_SESSION['errorMensaje'];
+            echo '</div>';
+            unset($_SESSION['errorMensaje']);
+        }
+
+        if (isset($_SESSION['successMensaje'])) {
+            echo '<div class="alert alert-success text-center alerta-fija" role="alert">';
+            echo $_SESSION['successMensaje'];
+            echo '</div>';
+            unset($_SESSION['successMensaje']);
+        }
+        ?>
+
         <div id="registro">
             <form action="registro.php" method="POST">
                 <picture>
@@ -35,8 +85,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="confirmar_password" class="form-label">Confirmar Contraseña</label>
-                    <input type="password" id="confirmar_password" name="confirmar_password" class="form-control"
-                        required>
+                    <input type="password" id="confirmar_password" name="confirmar_password" class="form-control" required>
                     <div id="mensaje-error" class="text-danger mt-2" style="display: none;">
                         Las contraseñas no coinciden.
                     </div>
@@ -44,32 +93,6 @@
                 <button type="submit" class="btn btn-primary">Registrar</button>
             </form>
         </div>
-
-
-        <?php
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            session_start();
-            include 'conexion.php';
-
-            $numero_empleado = rand(0, 100000);
-            $nombre = $_POST['nombre'];
-            $apellido = $_POST['apellido'];
-            $email = $_POST['email'];
-            $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-            $foto_default = './img/Usuario.svg';
-
-            try {
-                $sql = "INSERT INTO EMPLEADOS (NUMERO_EMPLEADO, NOMBRE, APELLIDO, EMAIL, PASSWORD, FOTO) VALUES (?, ?, ?, ?, ?, ?)";
-                $stmt = $conexion->prepare($sql);
-                $stmt->execute([$numero_empleado, $nombre, $apellido, $email, $password, $foto_default]);
-                echo 'Registro exitoso';
-            } catch (PDOException $e) {
-                echo 'Error: ' . $e->getMessage();
-            }
-        }
-        ?>
-
-
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
